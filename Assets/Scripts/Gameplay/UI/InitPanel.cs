18f8c385129602ee8b3d939d48f6fd2758aa6a6f
Mephilas <435495521@qq.com>
2024-05-13 22:39:49 +0800

    LOGO更新
    希望能在 4RE 上再次看到这两个LOGO：）

--- Assets/Scripts/Gameplay/UI/InitPanel.cs
+++ Assets/Scripts/Gameplay/UI/InitPanel.cs
@@ -1,17 +1,46 @@
+using DG.Tweening;
+
 /// <summary>
 /// 初始化面板
 /// </summary>
 public sealed class InitPanel : UIPanelBase
 {
+    private const int LOGO_FADE_DURATION = 3;
+
     protected override void Awake()
     {
         base.Awake();
 
-        Invoke(nameof(Begin), 3);
+        StartCoroutine(nameof(BeginC));
     }
 
     /// <summary>
     /// 阿伟你又在打电动哦
     /// </summary>
-    private void Begin() => GameManager_.Trigger(GameEventType.VideoCG, "0");
+    private System.Collections.IEnumerator BeginC()
+    {
+        yield return Const.WAIT_FOR_2S;
+
+        CGC<UnityEngine.UI.MaskableGraphic>("LogoSoftStar").DOFade(1, LOGO_FADE_DURATION);
+        CGC<UnityEngine.UI.MaskableGraphic>("Copyright").DOFade(1, LOGO_FADE_DURATION);
+
+        yield return Const.WAIT_FOR_3S;
+
+        CGC<UnityEngine.UI.MaskableGraphic>("LogoSoftStar").DOFade(0, LOGO_FADE_DURATION);
+        CGC<UnityEngine.UI.MaskableGraphic>("Copyright").DOFade(0, LOGO_FADE_DURATION);
+
+        yield return Const.WAIT_FOR_3S;
+
+        CGC<UnityEngine.UI.MaskableGraphic>("LogoCube").DOFade(1, LOGO_FADE_DURATION);
+        CGC<UnityEngine.UI.MaskableGraphic>("CopyrightCube").DOFade(1, LOGO_FADE_DURATION);
+
+        yield return Const.WAIT_FOR_3S;
+
+        CGC<UnityEngine.UI.MaskableGraphic>("LogoCube").DOFade(0, LOGO_FADE_DURATION);
+        CGC<UnityEngine.UI.MaskableGraphic>("CopyrightCube").DOFade(0, LOGO_FADE_DURATION);
+
+        yield return Const.WAIT_FOR_3S;
+
+        GameManager_.Trigger(GameEventType.VideoCG, "0");
+    }
 }
